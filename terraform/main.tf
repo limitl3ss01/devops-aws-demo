c733244d268b6e2ebc6d554edc5e605230734404
Limitless <kenon949@gmail.com>
2025-07-25 02:05:34 +0200

    AWS

---
+++ terraform/main.tf
@@ -0,0 +1,51 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = var.key_name
+  public_key = file("${path.module}/devops-aws-demo-key.pub")
+}
+
+resource "aws_security_group" "devops_sg" {
+  name        = "devops-aws-demo-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_default_vpc" "default" {}
+
+resource "aws_instance" "devops_ec2" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
+
+  tags = {
+    Name = "devops-aws-demo-ec2"
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+} 
